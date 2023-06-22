21820afe0d1bec2c4f2346a451e8753290f0ca38
Yarod Aliaga <yarod.lanas@gmail.com>
2023-06-21 21:55:01 -0400

    Se crea la función para calcular descuentos en base a porcentajes

--- funciones.py
+++ funciones.py
@@ -12,4 +12,32 @@ def calcular_iva():
     round(iva, 2)
     print(f'El IVA de tú producto es: ${iva}')
     
-calcular_iva()
+def calcular_descuento():
+    while True:
+        producto = input("Calcularemos el descuento de tú producto, por favor, ingresa el valor del mismo: ")
+        try:
+            producto = int(producto)
+            break
+        except:
+            print('Los datos no son validos, reintentar')
+            continue
+            #Termino While
+            
+    while True:
+        porcentaje = input("Por favor, ingresa el porcentaje de descuento: ")
+        try:
+            porcentaje = int(porcentaje)
+            break
+        except:
+            print('Datos no validos, reintentar')
+            continue
+
+    descuento = (producto * porcentaje) / 100
+    valor_final = producto - descuento
+
+    print(f'El valor de tú producto es de : ${producto}')
+    print(f'El porcentaje de tú producto es de : ${porcentaje}')
+    print(f'El descuento de tú producto es de : ${descuento}')
+    print(f"El valor final de tú producto es de: ${valor_final}")
+
+calcular_descuento()
